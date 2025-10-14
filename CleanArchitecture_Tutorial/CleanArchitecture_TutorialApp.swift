782bd3d318b6d14f8b1309a685d878a1a9736a7a
roy.lee2 <roy.lee2@kakaocorp.com>
2025-10-15 05:46:42 +0900

    Initial Commit

---
+++ CleanArchitecture_Tutorial/CleanArchitecture_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitecture_TutorialApp.swift
+//  CleanArchitecture_Tutorial
+//
+//  Created by 이병훈 on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitecture_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
