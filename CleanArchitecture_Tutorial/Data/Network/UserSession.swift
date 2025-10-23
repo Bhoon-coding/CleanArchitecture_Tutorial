6d99b068b8826608515466474903e4d1f3642b0d
bhooncoding <bhooncoding@gmail.com>
2025-10-24 06:23:04 +0900

    네트워크 request 레이어 설정

---
+++ CleanArchitecture_Tutorial/Data/Network/UserSession.swift
@@ -0,0 +1,35 @@
+//
+//  UserSession.swift
+//  CleanArchitecture_Tutorial
+//
+//  Created by 이병훈 on 10/24/25.
+//
+
+import Foundation
+
+import Alamofire
+
+public protocol SessionProtocol {
+    func request(_ convertible: URLConvertible, method: HTTPMethod, parameters: Parameters?, headers: HTTPHeaders?) -> DataRequest
+}
+
+class UserSession {
+    private var session: Session
+    
+    init() {
+        let config = URLSessionConfiguration.default
+        config.requestCachePolicy = .returnCacheDataElseLoad
+        self.session = Session(configuration: config)
+    }
+    
+    func request(
+    _ convertible: URLConvertible,
+    method: HTTPMethod = .get,
+    parameters: Parameters? = nil,
+    headers: HTTPHeaders? = nil
+    ) -> DataRequest {
+        return session.request(convertible, method: method, parameters: parameters)
+    }
+    
+    
+}
